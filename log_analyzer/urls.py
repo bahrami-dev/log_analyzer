1d66bbbf9a0c9f44fa8fbb675751830155fda6f9
Mahdi Bahrami <mahdi.bahrami74@gmail.com>
2023-09-05 15:55:53 +0330

    update urls

--- log_analyzer/urls.py
+++ log_analyzer/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('logs.urls')),
+    path('api/v1/logs/', include('logs.urls')),
 ]
--- logs/urls.py
+++ logs/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import FileUploadView
+from .views import FileUploadView, StatusCodeList
 
 urlpatterns = [
-    path('upload/', FileUploadView.as_view(), name='upload'),
+    path('upload', FileUploadView.as_view(), name='upload'),
+    path('', StatusCodeList.as_view(), name='status_code')
 ]
